0e07aba08f524efa64aaddf90a4dff2c8d23c225
Athiwat <whitenetstar@gmail.com>
2019-01-09 16:13:24 +0700

    update verify

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = 'W0CQtqyQTdztb4t3qB3hetp9XajHsZxcZdQIaMneO08yqOplm52a/2wRpzZ9vdCfC7Pf8Ewz/AC2ywE/bT0DE4mReApYY+HUAVNcVR2uqcIghYQzlhOtI3hEKZREhS95RAOYvgAkYPmqZ3BZSvoWIgdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/v2/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
