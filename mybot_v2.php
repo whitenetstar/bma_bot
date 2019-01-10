402965c65b203af60c571a51c02637f424e8eeb5
whitenetstar <whitenetstar@gmail.com>
2019-01-10 20:56:51 +0700

    new push mybot

--- mybot_v2.php
+++ mybot_v2.php
@@ -8,15 +8,10 @@ $uid = 'Ub7e92a80ec459cbb30ee1cb5fde78900';
 $bot = new \LINE\LINEBot(new CurlHTTPClient($access_token), [
     'channelSecret' => $channelSecret
 ]);
-$request_body = file_get_contents("php://input");
+//$request_body = file_get_contents("php://input");
 $signature = new \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE;
-try {
-            $events = $bot->parseEventRequest($request_body, $signature[0]);
-            } catch (InvalidSignatureException $e) {
-                return $e->getMessage();
-            } catch (InvalidEventRequestException $e) {
-                return $e->getMessage();
-            }
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ทดสอบนะค่ะ');
 $response = $bot->pushMessage($uid, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>
