8aa3ad7c0cc940fa1d61331a060b5838c751fc44
whitenetstar <whitenetstar@gmail.com>
2019-01-10 21:14:51 +0700

    Update mybot_v2.php

--- mybot_v2.php
+++ mybot_v2.php
@@ -5,8 +5,8 @@ require "vendor/autoload.php";
 $access_token = 'W0CQtqyQTdztb4t3qB3hetp9XajHsZxcZdQIaMneO08yqOplm52a/2wRpzZ9vdCfC7Pf8Ewz/AC2ywE/bT0DE4mReApYY+HUAVNcVR2uqcIghYQzlhOtI3hEKZREhS95RAOYvgAkYPmqZ3BZSvoWIgdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '695209a4c6640b2d57bbc437b15bdda8';
 $uid = 'Ub7e92a80ec459cbb30ee1cb5fde78900';
-//$content = file_get_contents('php://input');
-$signature = new \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE;
+$content = file_get_contents('php://input');
+//$signature = new \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE;
 $bot = new \LINE\LINEBot(new CurlHTTPClient($access_token), [
     'channelSecret' => $channelSecret
 ]);
