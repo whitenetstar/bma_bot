0f2f97a1cb91724069d15ace4842af69107be542
whitenetstar <whitenetstar@gmail.com>
2019-01-10 21:45:38 +0700

    Update mybot_v2.php

--- mybot_v2.php
+++ mybot_v2.php
@@ -1,12 +1,15 @@
 <?php
 
 require "vendor/autoload.php";
-
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient;
+use LINE\LINEBot\Constant\HTTPHeader;
 $access_token = 'W0CQtqyQTdztb4t3qB3hetp9XajHsZxcZdQIaMneO08yqOplm52a/2wRpzZ9vdCfC7Pf8Ewz/AC2ywE/bT0DE4mReApYY+HUAVNcVR2uqcIghYQzlhOtI3hEKZREhS95RAOYvgAkYPmqZ3BZSvoWIgdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '695209a4c6640b2d57bbc437b15bdda8';
 $uid = 'Ub7e92a80ec459cbb30ee1cb5fde78900';
 $content = file_get_contents('php://input');
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$signature = new \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE;
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('สวัสดีนะจ๊ะหนู');
 $response = $bot->pushMessage($uid, $textMessageBuilder);
